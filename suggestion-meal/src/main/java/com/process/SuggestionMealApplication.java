3155f39c5ae65dcb508bbf30402e447a3a3f5e24
Vuhq <vuhq@biplus.com.vn>
2023-04-12 14:13:11 +0700

    init

---
+++ suggestion-meal/src/main/java/com/process/SuggestionMealApplication.java
@@ -0,0 +1,13 @@
+package com.process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuggestionMealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuggestionMealApplication.class, args);
+	}
+
+}
